d21810e8c3ee562a7417ad05bf647705da2b1611
pnocedalopez <pnocedalopez@victoria15.danielcastelao.org>
2017-12-14 09:43:22 +0100

    LoVivido

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -5,38 +5,39 @@
 package lovivido;
 
 import javax.swing.JOptionPane;
+/**
+ * Calculo de los dias, meses y años en una cantidad de años introducida.
+ * @author pnocedalopez
+ */
 
 public class LoVivido {
+/**
+ * Metodo principal
+ * @param args 
+ */
+    public static void main(String[] args) {
+        
+        //Declaracion de variables e instanciacion de objetos.
+        String n, e;
+        int m, d, h, a;
+        Calculos calculos = new Calculos();
+        
+        //Entrada de datos.
+        n = JOptionPane.showInputDialog("Escriba su nombre: ");
+        e = JOptionPane.showInputDialog("Escriba su edad: ");
+        a = Integer.parseInt(e);
+        
+        //Calculos de días, meses y horas.
+        m = calculos.calculoM(a);
+        d = calculos.calculoD(a);
+        h = calculos.calculoH(d);
+        //Salidas.
+        JOptionPane.showMessageDialog(null, "Meses: " + m, "Numero de meses vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Días: " + d, "Numero de días vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Horas: " + h, "Numero de horas vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        //Fin.
+        System.exit(0);
 
-public static void main(String[] args) {
-String n;
-String e;
-
-int m;
-int d;
-int h;
-int a;
-
-n = JOptionPane.showInputDialog ("Escriba su nombre: ");
-e = JOptionPane.showInputDialog ("Escriba su edad: ");
- 
-a = Integer.parseInt (e);
- 
-m = (a*12);
-d = (a*365);
-h = (d*24);
- 
- JOptionPane.showMessageDialog(null,  "Meses: "  +m,"Numero de meses vividos de " +n,JOptionPane.INFORMATION_MESSAGE);
-
-
- JOptionPane.showMessageDialog(null, "Días: " +d,"Numero de días vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
-
-
- JOptionPane.showMessageDialog(null, "Horas: " +h,"Numero de horas vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
-
-   System.exit (0);
-
-}
-   
+    }
 
 }
