37e77d9c7f19d4972c16a9521c4cf68ad242982d
pnocedalopez <pnocedalopez@victoria15.danielcastelao.org>
2017-12-15 08:56:12 +0100

    LoVividoSalidas

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -32,12 +32,17 @@ public class LoVivido {
         d = calculos.calculoD(a);
         h = calculos.calculoH(d);
         //Salidas.
-        JOptionPane.showMessageDialog(null, "Meses: " + m, "Numero de meses vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
-        JOptionPane.showMessageDialog(null, "Días: " + d, "Numero de días vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
-        JOptionPane.showMessageDialog(null, "Horas: " + h, "Numero de horas vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        salida(m, d, h, n);
         //Fin.
         System.exit(0);
 
+    }
+    
+    public static void salida(int m, int d, int h, String n){
+        JOptionPane.showMessageDialog(null, "Meses: " + m, "Numero de meses vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Días: " + d, "Numero de días vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, "Horas: " + h, "Numero de horas vividos de " + n, JOptionPane.INFORMATION_MESSAGE);
+        
     }
 
 }
